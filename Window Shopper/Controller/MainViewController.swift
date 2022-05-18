5fed33dcc46899a6b617c78de45354343ec3cba4
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-18 15:32:54 +0700

    Adding logic to calculate hours and currency label to textField

--- Window Shopper/Controller/MainViewController.swift
+++ Window Shopper/Controller/MainViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var wageTextField: CurrencyTextField!
-    
     @IBOutlet weak var priceTextField: CurrencyTextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hourLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 80))
         
         calcBtn.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         calcBtn.setTitle("Calculate", for: .normal)
@@ -25,11 +26,27 @@ class MainViewController: UIViewController {
         
         wageTextField.inputAccessoryView = calcBtn
         priceTextField.inputAccessoryView = calcBtn
+        
+        resultLabel.isHidden = true
+        hourLabel.isHidden = true
     }
 
     @objc func calculator() {
-        
+        if let wageText = wageTextField.text, let priceText = priceTextField.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                view.endEditing(true)
+                resultLabel.isHidden = false
+                hourLabel.isHidden = false
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
 
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLabel.isHidden = true
+        hourLabel.isHidden = true
+        wageTextField.text = ""
+        priceTextField.text = ""
+    }
 }
 
--- Window Shopper/Views/CurrencyTextField.swift
+++ Window Shopper/Views/CurrencyTextField.swift
@@ -10,6 +10,23 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 10, y: (frame.size.height/2) - size/2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -23,6 +40,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
